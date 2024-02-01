eb74e2c34a9890b4ca1cb4f66ce2ca4b03e0fc17
andrkavr <72137459+andrkavr@users.noreply.github.com>
2024-02-01 14:33:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+# Introduction
+This is a tutorial project on how to create a fullstack e-commerce site using next.js, react.js, mongo, tailwind, styled components
---
+++ ecommerce-admin/pages/index.js
@@ -0,0 +1,25 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-700 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 px-4 rounded-lg"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      Logged in as {session.user.email} <br />
+      <button onClick={() => signOut()}>Log out</button>
+    </div>
+  );
+}
